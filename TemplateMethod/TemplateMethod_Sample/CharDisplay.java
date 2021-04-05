339569aa1a163c55de50aa252282a3f7db9152cb
ChaeEun Jeong <35367660+fkakd1205@users.noreply.github.com>
2021-04-05 10:47:02 +0900

    modify comment

--- TemplateMethod/TemplateMethod_Sample/CharDisplay.java
+++ TemplateMethod/TemplateMethod_Sample/CharDisplay.java
@@ -4,7 +4,7 @@ public class CharDisplay extends AbstractDisplay {  // CharDisplay는 AbstractDi
         this.ch = ch;                             // 필드에 기억해 둔다.
     }
     public void open() {                            // 상위 클래스에서는 추상 메소드였다.
-						  							// 여기에서 오버라이드해서 구현.
+						    // 여기에서 오버라이드해서 구현.
         System.out.print("<<");                     // 개시 문자열"<<"을 표시한다.
     }
     public void print() {                           // print 메소드도 여기에서 구현한다.

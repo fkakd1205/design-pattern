1661cc896438ae0e92501545207cc9eab22f80c0
fkakd1205 <fkakd0102@naver.com>
2021-04-11 13:57:36 +0900

    Bridge practice1 code

---
+++ Bridge/Bridge_A1/DisplayImpl.java
@@ -0,0 +1,6 @@
+// '구현의 클래스 계층'의 최상위 클래스
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+    public abstract void rawPrint();
+    public abstract void rawClose();
+}
---
+++ Bridge/Bridge_A1/RandomCountDisplay.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+// '기능의 클래스 계층'에 존재. CountDisplay 클래스에 기능을 추가
+public class RandomCountDisplay extends CountDisplay {
+    private Random random = new Random();
+    
+    public RandomCountDisplay(DisplayImpl impl) {
+        super(impl);
+    }
+    
+    public void randomDisplay(int times) {
+        multiDisplay(random.nextInt(times));	// random.nextInt(times)-0~times사이의 랜덤한 정수를 리턴. 
+    }
+}
